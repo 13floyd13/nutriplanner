f13c5e398097b71aa2149c0643f0de8199f3da9b
13floyd13 <martinpdev@protonmail.com>
2023-06-28 14:36:29 +0200

    chore(Log): :loud_sound: adding log

--- src/main/java/com/forrest/nutriplanner/controller/UserRestController.java
+++ src/main/java/com/forrest/nutriplanner/controller/UserRestController.java
@@ -9,6 +9,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +23,7 @@ import java.util.NoSuchElementException;
 @RequestMapping("/api/user")
 @Tag(name = "User")
 public class UserRestController {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserRestController.class);
     @Autowired
     UserServiceImpl userService;
 
@@ -45,6 +47,7 @@ public class UserRestController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         try {
             user = userService.saveUser(user);
+            logger.info("user created");
             return ResponseEntity.status(HttpStatus.CREATED).body(user);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -90,7 +93,7 @@ public class UserRestController {
     @Operation(summary = "Get User by id")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "201", description = "OK",
+                    @ApiResponse(responseCode = "200", description = "OK",
                             content = {@Content(schema = @Schema(implementation = User.class))}),
                     @ApiResponse(responseCode = "400", description = "Invalid input",
                             content = @Content),
@@ -118,7 +121,7 @@ public class UserRestController {
     @Operation(summary = "Get all clients")
     @ApiResponses(
             value = {
-                    @ApiResponse(responseCode = "201", description = "OK",
+                    @ApiResponse(responseCode = "200", description = "OK",
                             content = {@Content(schema = @Schema(implementation = User.class))}),
                     @ApiResponse(responseCode = "400", description = "Invalid input",
                             content = @Content),
@@ -156,6 +159,7 @@ public class UserRestController {
     public ResponseEntity<Void> deleteUserById(@PathVariable("id") Long id) {
         try {
             userService.getUserById(id);
+            logger.info("userToDeleteFind");
             userService.deleteUserById(id);
             return ResponseEntity.noContent().build();
         } catch (NoSuchElementException e) {
